771e6d83a44a61b4ab903291f7c048391e23654e
Pavel Slabý <pavel.slaby@devcast.cz>
2019-06-16 23:05:25 +0200

    Fix multicurency settings double form

--- app/overrides/multi_currency_admin_settings.rb
+++ app/overrides/multi_currency_admin_settings.rb
@@ -2,6 +2,6 @@
 
 Deface::Override.new(virtual_path: 'spree/admin/general_settings/edit',
                      name: 'multiple-currency',
-                     insert_after: Spree.version.to_f < 3.5 ? '.panel.currency' : '.form-group',
+                     insert_after: Spree.version.to_f < 3.5 ? '.panel.currency' : '.panel.currency .form-group',
                      partial: 'spree/admin/general_settings/form',
                      disabled: false)
